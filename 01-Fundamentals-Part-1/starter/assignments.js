7001469f8c133b48ca16cd5d95c6e88de9ce259e
ttowntom <117744552+ttowntom@users.noreply.github.com>
2023-09-19 20:03:47 +0200

    11. data types

---
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -0,0 +1,21 @@
+// Lectuer 1. Values and Variables
+let country = "Norway";
+let continent = "Europe";
+let population = 5.4;
+
+console.log(
+	country +
+		" is in " +
+		continent +
+		" and has a population of " +
+		population +
+		" million people."
+);
+
+// Lecture 2. Data Types
+let isIsland = false;
+let language;
+console.log(typeof isIsland);
+console.log(typeof population);
+console.log(typeof country);
+console.log(typeof language);
---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,29 @@
+/* let js = "amazing";
+console.log(40 + 8 + 23 - 10);
+
+console.log("TR");
+console.log(23);
+
+let firstName = "tTownTom";
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+*/
+
+// 2. Data Types
+console.log(typeof true);
+console.log(typeof 23);
+console.log(typeof "true");
+
+let javascriptIsFun = true;
+javascriptIsFun = "YES!";
+console.log(javascriptIsFun);
+console.log(typeof javascriptIsFun);
+
+let year;
+console.log(year);
+console.log(typeof year);
+year = 1991;
+console.log(typeof year);
+
+console.log(typeof null); // an object, but should be null
