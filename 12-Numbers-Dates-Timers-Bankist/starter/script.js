40bf1deac220636a2558ea40eb66f581e03b936c
ttowntom <git.running054@passinbox.com>
2024-06-13 14:22:28 +0200

    176

--- 12-Numbers-Dates-Timers-Bankist/starter/script.js
+++ 12-Numbers-Dates-Timers-Bankist/starter/script.js
@@ -252,74 +252,189 @@ btnSort.addEventListener('click', function (e) {
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
 // LECTURES
-
-// console.log(23 === 23.0);
-// console.log(0.1 + 0.2);
-// console.log(0.1 + 0.2 === 0.3);
+console.log(23 === 23.0);
+console.log(0.1 + 0.2);
+console.log(0.1 + 0.2 === 0.3);
 
 // // Conversion
-// console.log(Number('23'));
-// console.log(+'23');
+console.log(Number('23'));
+console.log(+'23');
 
 // // Parsing
-// console.log(Number.parseInt('30px', 10));
-// console.log(Number.parseFloat('2.5rem'));
+console.log(Number.parseInt('30px', 10));
+console.log(Number.parseFloat('2.5rem'));
 
 // // Check if value is NaN
-// console.log(Number.isNaN(20));
-// console.log(Number.isNaN('20'));
-// console.log(Number.isNaN(+'20X'));
-// console.log(Number.isNaN(23 / 0));
+console.log(Number.isNaN(20));
+console.log(Number.isNaN('20'));
+console.log(Number.isNaN(+'20X'));
+console.log(Number.isNaN(23 / 0));
 
 // // Check if value is number
-// console.log(Number.isFinite(20));
-// console.log(Number.isFinite('20'));
-// console.log(Number.isFinite(+'20X'));
-// console.log(Number.isFinite(23 / 0));
+console.log(Number.isFinite(20));
+console.log(Number.isFinite('20'));
+console.log(Number.isFinite(+'20X'));
+console.log(Number.isFinite(23 / 0));
 
 // // Check if value is integer
-// console.log(Number.isInteger(20));
-// console.log(Number.isInteger(20.0));
-// console.log(Number.isInteger(20.1));
-// console.log(Number.isInteger('20'));
-*/
+console.log(Number.isInteger(20));
+console.log(Number.isInteger(20.0));
+console.log(Number.isInteger(20.1));
+console.log(Number.isInteger('20'));
+
 
 // // Math and Rounding
-// console.log(Math.sqrt(25));
-// console.log(25 ** (1 / 2));
-// console.log(8 ** (1 / 3));
+console.log(Math.sqrt(25));
+console.log(25 ** (1 / 2));
+console.log(8 ** (1 / 3));
 
-// console.log(Math.max(5, 18, 23, 11, 2));
-// console.log(Math.max(5, 18, '23', 11, 2));
-// console.log(Math.max(5, 18, '23px', 11, 2));
+console.log(Math.max(5, 18, 23, 11, 2));
+console.log(Math.max(5, 18, '23', 11, 2));
+console.log(Math.max(5, 18, '23px', 11, 2));
 
-// console.log(Math.min(5, 18, 23, 11, 2));
+console.log(Math.min(5, 18, 23, 11, 2));
 
-// console.log(Math.PI * Number.parseFloat('10px') ** 2);
+console.log(Math.PI * Number.parseFloat('10px') ** 2);
 
-// console.log(Math.trunc(Math.random() * 6) + 1);
+console.log(Math.trunc(Math.random() * 6) + 1);
 
-// const randomInt = (min, max) =>
-//   Math.floor(Math.random() * (max - min) + 1) + min;
+const randomInt = (min, max) =>
+  Math.floor(Math.random() * (max - min) + 1) + min;
 
-// console.log(randomInt(10, 20));
+console.log(randomInt(10, 20));
 
 // // Rounding integers
-// console.log(Math.trunc(23.3));
+console.log(Math.trunc(23.3));
 
-// console.log(Math.round(23.3));
-// console.log(Math.round(23.9));
+console.log(Math.round(23.3));
+console.log(Math.round(23.9));
 
-// console.log(Math.ceil(23.3));
-// console.log(Math.ceil(23.9));
+console.log(Math.ceil(23.3));
+console.log(Math.ceil(23.9));
 
-// console.log(Math.floor(23.3));
-// console.log(Math.floor('23.9'));
+console.log(Math.floor(23.3));
+console.log(Math.floor('23.9'));
 
-// console.log(Math.trunc(-23.3));
-// console.log(Math.floor(-23.3));
+console.log(Math.trunc(-23.3));
+console.log(Math.floor(-23.3));
 
 // // Rounding decimals
-// console.log((2.7).toFixed(0));
-// console.log((2.7).toFixed(3));
-// console.log(+(2.345).toFixed(2));
+console.log((2.7).toFixed(0));
+console.log((2.7).toFixed(3));
+console.log(+(2.345).toFixed(2));
+
+
+// // Remainder operator
+console.log(5 % 2);
+console.log(5 / 2); // 5 = 2 * 2 + 1
+
+console.log(8 % 3);
+console.log(8 / 3); // 8 = 2 * 3 + 2
+
+console.log(6 % 2);
+console.log(6 / 2); // 6 = 3 * 2 + 0
+
+const isEven = n => n % 2 === 0;
+console.log(isEven(8));
+console.log(isEven(23));
+console.log(isEven(514));
+
+labelBalance.addEventListener('click', function () {
+  [...document.querySelectorAll('.movements__row')].forEach(function (row, i) {
+    if (i % 2 === 0) row.style.backgroundColor = 'orangered';
+    if (i % 3 === 0) row.style.backgroundColor = 'blue';
+  });
+});
+
+///////////////////////////////////////
+// Numeric Separators
+// 287,460,000,000
+const diameter = 287_460_000_000;
+console.log(diameter);
+
+const price = 345_99;
+console.log(price);
+
+const transferFee1 = 15_00;
+const transferFee2 = 1_500;
+
+const PI = 3.1415;
+console.log(PI);
+
+console.log(Number('230_000'));
+console.log(parseInt('230_000'));
+
+///////////////////////////////////////
+
+
+// // Working with BigInt
+console.log(2 ** 53 - 1);
+console.log(Number.MAX_SAFE_INTEGER);
+
+console.log(2 ** 53 + 1);
+console.log(2 ** 53 + 2);
+console.log(2 ** 53 + 3);
+
+console.log(483897983459873498573498573498578934758n);
+console.log(BigInt(483897983459873498573498573498578934758));
+
+// // Operations
+console.log(10000n + 10000n);
+console.log(483897983459873498573498573498578934758n * 1000000n);
+// console.log(Math.sqrt(16n));
+
+const huge = 483897983459873498573498573498578934758n;
+const num = 23;
+console.log(huge * BigInt(num));
+
+// // Exceptions
+console.log(20n > 15);
+console.log(20n === 20);
+console.log(typeof 20n);
+console.log(20n == 20);
+
+console.log(huge + ' is REALLY big!!!');
+
+// // Divisions
+console.log(10n / 3n);
+console.log(10 / 3);
+
+
+///////////////////////////////////////
+// Creating Dates
+
+// // Create a date
+const now = new Date();
+console.log(now);
+
+console.log(new Date('Aug 02 2020 18:05:41'));
+console.log(new Date('December 24, 2015'));
+console.log(new Date(account1.movementsDates[0]));
+
+console.log(new Date(2037, 10, 19, 15, 23, 5));
+console.log(new Date(2037, 10, 31));
+
+console.log(new Date(0));
+console.log(new Date(3 * 24 * 60 * 60 * 1000));
+
+
+// // Working with dates
+const future = new Date(2037, 10, 19, 15, 23);
+console.log(future);
+console.log(future.getFullYear());
+console.log(future.getMonth());
+console.log(future.getDate());
+console.log(future.getDay());
+console.log(future.getHours());
+console.log(future.getMinutes());
+console.log(future.getSeconds());
+console.log(future.toISOString());
+console.log(future.getTime());
+
+console.log(new Date(2142237180000));
+
+console.log(Date.now());
+
+future.setFullYear(2040);
+console.log(future);
+*/
