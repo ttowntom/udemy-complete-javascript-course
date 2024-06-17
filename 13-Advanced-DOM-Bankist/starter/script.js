daf42226872c8f4dd1bda615eff728850b1d206d
ttowntom <git.running054@passinbox.com>
2024-06-17 15:02:04 +0200

    190

--- 13-Advanced-DOM-Bankist/starter/script.js
+++ 13-Advanced-DOM-Bankist/starter/script.js
@@ -78,8 +78,8 @@ document
 message.style.backgroundColor = '#37383d';
 message.style.width = '106%';
 
-console.log(getComputedStyle(message).color);
-console.log(getComputedStyle(message).height);
+// console.log(getComputedStyle(message).color);
+// console.log(getComputedStyle(message).height);
 
 message.style.height =
   Number.parseFloat(getComputedStyle(message).height, 10) + 30 + 'px';
@@ -88,19 +88,19 @@ document.documentElement.style.setProperty('--color-primary', 'orangered');
 
 // Attributes
 const logo = document.querySelector('.nav__logo');
-console.log(logo.alt);
-console.log(logo.src);
-console.log(logo.className);
+// console.log(logo.alt);
+// console.log(logo.src);
+// console.log(logo.className);
 
 logo.alt = 'Beautiful minimalist logo';
 
 // Non-standard
-console.log(logo.designer);
-console.log(logo.getAttribute('designer'));
+// console.log(logo.designer);
+// console.log(logo.getAttribute('designer'));
 logo.setAttribute('company', 'Bankist');
 
 // Data attributes
-console.log(logo.dataset.versionNumber);
+// console.log(logo.dataset.versionNumber);
 
 // Classes
 logo.classList.add('c');
@@ -110,3 +110,47 @@ logo.classList.contains('c');
 
 // Don't use
 // logo.className = 'jonas';
+
+// Smooth scrolling
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+const section1 = document.querySelector('#section--1');
+
+btnScrollTo.addEventListener('click', function (e) {
+  const s1coords = section1.getBoundingClientRect();
+  // console.log(s1coords);
+  // console.log(e.target.getBoundingClientRect());
+  // console.log('Current scroll (X/Y)', window.pageXOffset, window.pageYOffset);
+  // console.log(
+  //   'height/width viewport',
+  //   document.documentElement.clientHeight,
+  //   document.documentElement.clientWidth
+  // );
+
+  // Scrolling
+  // window.scrollTo(
+  //   s1coords.left + window.pageXOffset,
+  //   s1coords.top + window.pageYOffset
+  // );
+
+  // window.scrollTo({
+  //   left: s1coords.left + window.pageXOffset,
+  //   top: s1coords.top + window.pageYOffset,
+  //   behavior: 'smooth',
+  // });
+
+  section1.scrollIntoView({ behavior: 'smooth' });
+});
+
+const h1 = document.querySelector('h1');
+
+const alertH1 = function (e) {
+  alert('addEventListener: Great! You are reading the heading :D');
+};
+
+h1.addEventListener('mouseenter', alertH1);
+
+// h1.onmouseenter = function (e) {
+//   alert('onmouseenter: Great! You are reading the heading :D');
+// };
+
+setTimeout(() => h1.removeEventListener('mouseenter', alertH1), 3000);
