38890c53fa67ff3f42589d1ff2f243e5b877a628
ttowntom <torgeir.riseth@ttowntom.com>
2024-06-27 12:59:35 +0200

    292

--- 18-forkify/starter/src/js/controller.js
+++ 18-forkify/starter/src/js/controller.js
@@ -28,11 +28,14 @@ const renderSpinner = function (parentEl) {
 
 const showRecipe = async function () {
   try {
+    const id = window.location.hash.slice(1);
+    if (!id) return;
+
     // Loading recipe
     renderSpinner(recipeContainer);
 
     const res = await fetch(
-      'https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886'
+      `https://forkify-api.herokuapp.com/api/v2/recipes/${id}`
     );
     const data = await res.json();
     if (!res.ok) throw new Error(`${data.message} (${res.status})`);
@@ -154,4 +157,5 @@ const showRecipe = async function () {
   }
 };
 
-showRecipe();
+// Listen for hash id
+['hashchange', 'load'].forEach(ev => window.addEventListener(ev, showRecipe));
