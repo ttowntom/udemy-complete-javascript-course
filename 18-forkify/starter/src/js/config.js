03fe4bbf504b9a5b7ff7ac2215b812a2ccd7f932
ttowntom <git.running054@passinbox.com>
2024-07-05 13:20:40 +0200

    310

--- 18-forkify/starter/src/js/config.js
+++ 18-forkify/starter/src/js/config.js
@@ -1,3 +1,5 @@
 export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
 export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 10;
+export const KEY = 'e03d3eee-015a-42b6-990e-0ba9f0d6bb7b';
+export const MODAL_CLOSE_SEC = 2.5;
